017e6fdb8119d5ef001f7cc1aaa6103302145a49
Your Name <edyatl@yandex.ru>
2026-04-25 00:47:22 +0300

    fix: Fix App.tsx - make it a component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
-import ReactDOM from 'react-dom/client';
 import UsageStats from './features/usage/components/UsageStats';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <UsageStats />
-  </QueryClientProvider>
-);
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UsageStats />
+    </QueryClientProvider>
+  );
+}
